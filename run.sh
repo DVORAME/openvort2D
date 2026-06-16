9d30664be029ff93f636b24c97cdf9c339443ce7
DVORAME <86205591@cuni.cz>
2026-06-16 09:54:44 +0200

    Add SLURM params

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
+#SBATCH -partition=gpu-ffa
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=16G
+#SBATCH --gpus=1
+#SBATCH --output=output_%j/log.txt
+#SBATCH --error=output_%j/err.txt
+#SBATCH --time=01:00:00
+
+
 alpha=0.061
 alphap=0.01746
 D=0.1
@@ -12,4 +21,4 @@ v_probe=25
 freq=2000
 
 python src/main.py --D $D --dt=$dt --alpha $alpha --alphap $alphap --walls --save --save-every 20 \
-	--tmax $tmax --pinning-v $v_pin --probe-v $v_probe --probe-v-freq $freq --N $N --no-plot --gpu 
+	--tmax $tmax --pinning-v $v_pin --probe-v $v_probe --probe-v-freq $freq --N $N --no-plot --gpu --output output_$SLURM_JOB_ID/out
