9d0da5503449a48c155e3aa7cb913946b71c5a83
DVORAME <86205591@cuni.cz>
2026-07-15 22:03:06 +0200

    Test avalanches

--- run_avalanches.sh
+++ run_avalanches.sh
@@ -5,8 +5,9 @@
 #SBATCH --mem=32G
 #SBATCH --time=12:00:00
 #SBATCH --job-name=openvort_avalanches
-#SBATCH --output=output_avalanches/output_avalanches.out
-#SBATCH --error=output_avalanches/error_avalanches.err
+#SBATCH --output=output_avalanches/output_%a.out
+#SBATCH --error=output_avalanches/error_%a.err
+#SBATCH --array=1-5
 
-pyenv exec python src/main.py --N 600 --tmax 100 --dt 1e-5 --pin-type drag --D 0.1 --circle --omega-ex '2*600*KAPPA/np.pi/D**2*(1-2*600*KAPPA/np.pi/D**2*t/200/2/np.pi)' --pinning-v-ex 600*KAPPA/np.pi*0.9 \
-	--polarization-type skewed --polarization 1 --save --save-every 1000 --no-plot --no-plot-save --output output_avalanches --gpu --load
+pyenv exec python src/main.py --N 600 --tmax 33 --dt 1e-5 --pin-type drag --D 0.1 --circle --omega-ex '2*600*KAPPA/np.pi/D**2*(1-2*600*KAPPA/np.pi/D**2*t/200/2/np.pi)' --pinning-v-ex "600*KAPPA/np.pi/10*$SLURM_ARRAY_TASK_ID" \
+	--polarization-type skewed --polarization 1 --save --save-every 1000 --no-plot --no-plot-save --output output_avalanches/output_$SLURM_ARRAY_TASK_ID --gpu --load
