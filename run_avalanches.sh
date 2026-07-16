008284955d912f1de4026aa6898f858e73bd8155
DVORAME <86205591@cuni.cz>
2026-07-16 19:56:31 +0200

    Test avalanches

--- run_avalanches.sh
+++ run_avalanches.sh
@@ -9,5 +9,5 @@
 #SBATCH --error=output_avalanches/error_%a.err
 #SBATCH --array=1-5
 
-pyenv exec python src/main.py --N 600 --tmax 33 --dt 1e-5 --pin-type drag --D 0.1 --circle --omega-ex '2*600*KAPPA/np.pi/D**2*(1-2*600*KAPPA/np.pi/D**2*t/200/2/np.pi)' --pinning-v-ex "600*KAPPA/np.pi/10*$SLURM_ARRAY_TASK_ID" \
+pyenv exec python src/main.py --N 600 --tmax 33 --dt 1e-5 --pin-type drag --D 0.1 --circle --omega-ex '2*600*KAPPA/np.pi/D**2*(1-2*600*KAPPA/np.pi/D**2*t/200/2/np.pi)' --pinning-v-ex "600*KAPPA/D/np.pi/500*$SLURM_ARRAY_TASK_ID" \
 	--polarization-type skewed --polarization 1 --save --save-every 1000 --no-plot --no-plot-save --output output_avalanches/output_$SLURM_ARRAY_TASK_ID --gpu --load
