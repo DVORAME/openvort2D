399fe0e8975653db82bae269a4d45a4891d52386
DVORAME <86205591@cuni.cz>
2026-08-07 11:00:06 +0200

    Fix grapher

--- src/graph_last.py
+++ src/graph_last.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--i', type=str, required=True, help='Input directory path')
 
-	parser.parse_args()
+	args = parser.parse_args()
 
-	vp_files = glob(path.join(parser.i, 'vp_*.npz'))
+	vp_files = glob(path.join(args.i, 'vp_*.npz'))
 	vp_files.sort()
 	graph_file = np.load(vp_files[-1], allow_pickle=True)
 	vp = graph_file['arr_0'].item()
-	plt.plot(vp['xs'], vp['ys'], 'o', ms = 2)
-	plt.savefig(path.join(parser.i, 'graph_last.png'), dpi=300)
+	plt.plot(vp.xs, vp.ys, 'o', ms = 2)
+	plt.savefig(path.join(args.i, 'graph_last.png'), dpi=300)
