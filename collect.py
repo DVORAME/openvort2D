0306f09c6a4e4bcf4da14ef9b4879151bfcad781
DVORAME <86205591@cuni.cz>
2026-08-08 09:43:03 +0200

    Change collect script

--- collect.py
+++ collect.py
@@ -2,14 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-for i in range(1, 6):
+D = 0.183146054323898
+KAPPA = 9.96e-4
+
+for i in range(1, 2):
 	# Load the CSV file into a DataFrame
 	df = pd.read_csv(f'output_avalanches/output_{i}/out.csv')
 
 	# Plotting the data
-	plt.subplot(3, 2, i)
-	plt.plot(df['t'], df['N'])
+	# plt.subplot(5, 2, i)
+	plt.plot(df['t'], df['N'], label='N')
+	plt.plot(df['t'], df['omega'] * np.pi * D**2 / 2 / KAPPA, label='N_exp')
 	plt.title(f'Output {i}')
 
+plt.legend()
 plt.tight_layout()
-plt.savefig('output_avalanches/N_t.png')
+plt.savefig('output_avalanches/N_t_1.png')
